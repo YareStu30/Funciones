6a1b5406f614d52df4b232e58810069c3264f74b
yarenla <yare.cs22@gmail.com>
2022-07-16 02:03:03 -0400

    add new colors and function to letters

--- assets/js/4script.js
+++ assets/js/4script.js
@@ -1,22 +1,32 @@
 /* Var*/
 let section1 = document.querySelector("#section1");
-section1.style.backgroundColor = "purple";
+section1.style.backgroundColor = "blueviolet";
 let section2 = document.querySelector("#section2");
-section1.style.backgroundColor = "aqua";
+section2.style.backgroundColor = "aqua";
 let section3 = document.querySelector("#section3");
-section1.style.backgroundColor = "gold";
+section3.style.backgroundColor = "gold";
 let section4 = document.querySelector("#section4");
-section1.style.backgroundColor = "blue";
+section4.style.backgroundColor = "blue";
 
 let changeColor = "";
 
 document.addEventListener("keydown", function (event) {
   if (event.key === "a") {
-    changeColor = "orange";
+    changeColor = "purple";
   } else if (event.key === "s") {
-    changeColor = "green";
+    changeColor = "orange";
   } else if (event.key === "d") {
     changeColor = "pink";
   }
   console.log(event.key);
+  changeClick(section1, changeColor);
+  changeClick(section2, changeColor);
+  changeClick(section3, changeColor);
+  changeClick(section4, changeColor);
 });
+
+let changeClick = (element, color) => {
+  element.addEventListener("click", (e) => {
+    element.style.backgroundColor = color;
+  });
+};
